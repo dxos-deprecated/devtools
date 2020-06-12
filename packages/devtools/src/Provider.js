502d91f16b3d333f613a0fee7b55da31c9b7ea10
Max Fierro <elmasse@gmail.com>
2020-06-12 13:15:18 -0300

    Sync Client detection -> API -> Devtool.

--- packages/devtools/src/Provider.js
+++ packages/devtools/src/Provider.js
@@ -2,15 +2,35 @@
 // Copyright 2020 DxOS.
 //
 
-import React, { useContext } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 
 export const Context = React.createContext({ });
 
 export default function Provider ({ bridge, children }) {
+  const [initialized, setInitialized] = useState(false);
+  const [ready, setReady] = useState(false);
+
+  useEffect(() => {
+    bridge.on('api', (ready) => {
+      setReady(ready);
+      setInitialized(true);
+    });
+  }, [bridge]);
+
   return (
-    <Context.Provider value={{ bridge }}>
-      {children}
-    </Context.Provider>
+    <>
+      {!initialized && (
+        <div>Waiting for DXOS client...</div>
+      )}
+      {ready && (
+        <Context.Provider value={{ bridge }}>
+          {children}
+        </Context.Provider>
+      )}
+      {(!ready && initialized) && (
+        <div>DXOS client Not Found.</div>
+      )}
+    </>
   );
 }
 
---
+++ packages/extension/src/main-panel/bridge.js
@@ -0,0 +1,31 @@
+
+import EventEmitter from 'events';
+import Bridge from 'crx-bridge';
+
+Bridge.setNamespace('dxos.devtools');
+Bridge.allowWindowMessaging('dxos.devtools');
+
+export default class BridgeProxy extends EventEmitter {
+  constructor () {
+    super();
+    this._init();
+  }
+
+  _init () {
+    Bridge.onMessage('api.ready', () => {
+      this.emit('api', true);
+    });
+
+    Bridge.onMessage('api.timeout', () => {
+      this.emit('api', false);
+    });
+  }
+
+  async send (message, payload = {}) {
+    return Bridge.sendMessage(message, payload, 'window');
+  }
+
+  listen (message, fn) {
+    Bridge.onMessage(message, fn);
+  }
+}
