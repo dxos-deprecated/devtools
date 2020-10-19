7f329ffd81380c25362764108ec620eed32bb45c
Dmytro Maretskyi <maretskii@gmail.com>
2020-10-19 13:31:15 +0200

    Lint

--- packages/devtools/src/containers/ItemsViewer.js
+++ packages/devtools/src/containers/ItemsViewer.js
@@ -1,14 +1,14 @@
-import React, { useEffect, useState } from 'react'
-import { useBridge } from '../hooks/bridge'
+import React, { useEffect, useState } from 'react';
+import { useBridge } from '../hooks/bridge';
 import JsonTreeView from '@dxos/react-ux/dist/es/components/JsonTreeView';
 
-export default function ItemsViewer() {
+export default function ItemsViewer () {
   const [bridge] = useBridge();
-  
+
   const [data, setData] = useState({});
   useEffect(() => {
     bridge.send('echo.items').then(data => setData(data));
-  }, [])
+  }, []);
 
   return (
     <JsonTreeView
@@ -16,5 +16,5 @@ export default function ItemsViewer() {
       data={data}
       depth={4}
     />
-  )
+  );
 }
--- packages/extension/src/devtools-client-api/handlers/items.js
+++ packages/extension/src/devtools-client-api/handlers/items.js
@@ -7,26 +7,26 @@ import { humanize } from '@dxos/crypto';
 export default ({ hook, bridge }) => {
   bridge.onMessage('echo.items', () => {
     const parties = hook.client.echo.queryParties().value;
-    console.log(parties)
+    console.log(parties);
 
-    // TODO(marik-d): Display items hierarhically 
-    const res = {}
-    for(const party of parties) {
+    // TODO(marik-d): Display items hierarhically
+    const res = {};
+    for (const party of parties) {
       const partyInfo = {};
       res[`Party ${humanize(party.key)}`] = partyInfo;
 
       const items = party.database.queryItems().value;
-      for(const item of items) {
-        const modelName = item.model.__proto__.constructor.name;
+      for (const item of items) {
+        const modelName = Object.getPrototypeOf(item.model).constructor.name;
         partyInfo[`${modelName} ${item.type} ${humanize(item.id)}`] = {
           id: humanize(item.id),
           type: item.type,
           modelType: item.model._meta.type,
-          modelName,
-        }
+          modelName
+        };
       }
     }
-    
+
     return res;
   });
 };
