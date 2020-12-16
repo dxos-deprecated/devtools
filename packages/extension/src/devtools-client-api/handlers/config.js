ccd54f1d3b379fb0d406daaafcad43e4a7d15d4d
rzadp <roopert7@gmail.com>
2020-12-16 16:54:22 +0100

    Add username to config section in devtools

--- packages/extension/src/devtools-client-api/handlers/config.js
+++ packages/extension/src/devtools-client-api/handlers/config.js
@@ -5,7 +5,13 @@
 export default ({ hook, bridge }) => {
   bridge.onMessage('config', () => {
     try {
-      return JSON.parse(JSON.stringify(hook.client.config)); // make sure the config is serializable
+      return {
+        config: JSON.parse(JSON.stringify(hook.client.config)), // make sure the config is serializable
+        profile: {
+          username: hook.client.getProfile()?.username,
+          publicKey: hook.client.getProfile()?.publicKey.toString()
+        }
+      };
     } catch (e) {
       console.error('DXOS DevTools: Config handler failed to respond');
       console.log(e);
