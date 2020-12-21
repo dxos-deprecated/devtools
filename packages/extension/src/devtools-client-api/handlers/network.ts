986f8411fac413c28edd6ef7aa205f8677455efa
rzadp <roopert7@gmail.com>
2020-12-21 12:22:33 +0100

    Add network connection

--- packages/extension/src/devtools-client-api/handlers/network.ts
+++ packages/extension/src/devtools-client-api/handlers/network.ts
@@ -9,13 +9,16 @@ import { DevtoolsContext } from '@dxos/client/dist/src/devtools-context';
 async function subscribeToNetworkStatus (hook: DevtoolsContext, stream: Stream) {
   async function update () {
     try {
-      stream.send(hook.networkManager.signal.getStatus());
+      const status = hook.networkManager.signal.getStatus();
+      console.log('status in update:', status);
+      stream.send(status);
     } catch (err) {
       console.error('DXOS DevTools: Network status update error');
       console.error(err);
     }
   }
   try {
+    console.log('network subscribing to status changed events');
     hook.networkManager.signal.statusChanged.on(update);
     await update();
   } catch (e) {
