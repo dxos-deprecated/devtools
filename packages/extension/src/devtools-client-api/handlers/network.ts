f7589b72771acf84a17beed65c0ead50c9ac8494
rzadp <roopert7@gmail.com>
2020-12-21 12:18:51 +0100

    Initial work on network bridge

--- packages/extension/src/devtools-client-api/handlers/network.ts
+++ packages/extension/src/devtools-client-api/handlers/network.ts
@@ -24,40 +24,7 @@ async function subscribeToNetworkStatus (hook: DevtoolsContext, stream: Stream) 
   }
 }
 
-// async function subscribeToNetworkTopics (hook: DevtoolsContext, stream: Stream) {
-//   async function update () {
-//     try {
-//       const res = await getData(client);
-//       console.log(res);
-//       stream.send(res);
-//     } catch (err) {
-//       console.error('DXOS DevTools: Snapshots update error');
-//       console.error(err);
-//     }
-//   }
-
-//   try {
-//     await hook.client.initialize();
-//     const unsubscribe = hook.networkManager.subscribe(() => {
-//       update();
-//     });
-
-//     stream.onClose(() => {
-//       unsubscribe();
-//     });
-
-//     update();
-//   } catch (e) {
-//     console.error('DXOS DevTools: Network handler failed to subscribe.');
-//     console.error(e);
-//   }
-// }
-
 export default ({ hook, bridge }: {hook: DevtoolsContext, bridge: typeof Bridge }) => {
-  // bridge.onOpenStreamChannel('network.topics', (stream) => {
-  //   subscribeToNetworkTopics(hook, stream);
-  // });
-
   bridge.onOpenStreamChannel('network.signal.status', (stream) => {
     subscribeToNetworkStatus(hook, stream);
   });
