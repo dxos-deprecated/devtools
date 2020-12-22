8ef381fdf3de04d4f53035a69326194174eef542
Przemyslaw Rzad <przemek@ethworks.io>
2020-12-22 11:35:39 +0100

    chore: Migrate devtools (#46)

--- packages/extension/src/background/index.js
+++
@@ -1,9 +0,0 @@
-//
-// Copyright 2020 DXOS.org
-//
-
-// REQUIRED FOR THE BRIDGE TO WORK.
-import Bridge from 'crx-bridge';
-
-Bridge.setNamespace('dxos.devtools');
-Bridge.allowWindowMessaging('dxos.devtools');
